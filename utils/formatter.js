85ff488150ebb7c6777b72965bd439e839295740
ramdhani136 <ramdhaniit@gmail.com>
2022-01-22 11:49:55 +0700

    upload

---
+++ utils/formatter.js
@@ -0,0 +1,16 @@
+const phoneNumberFormatter = (number) => {
+  // menghilangkan karakter selain angka
+  let formatted = number.replace(/\D/g, "");
+  //   menghilangkan angka 0 didepan dan di ganti dengan 62
+  if (formatted.startsWith("0")) {
+    formatted = "62" + formatted.substr(1);
+  }
+
+  if (!formatted.endsWith("@c.us")) {
+    formatted += "@c.us";
+  }
+
+  return formatted;
+};
+
+module.exports = { phoneNumberFormatter };
